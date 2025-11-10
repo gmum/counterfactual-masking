c80a7eef4416d35ebfed75bb4c3d1348bc3399ab
lukaszjanisiow <lukaszjanisiow@gmail.com>
2025-11-10 01:28:53 +0100

    Update model directory paths

--- source/train_classification.py
+++ source/train_classification.py
@@ -105,7 +105,7 @@ def main():
     epochs = 300
     batch_size = args.batch_size
 
-    os.makedirs(f"gin_{args.dataset.lower()}", exist_ok=True)
+    os.makedirs(f"models/gin_{args.dataset.lower()}", exist_ok=True)
 
     data_module = get_data_module(args.dataset)
     split = data_module.get_split(seed = seed)
@@ -144,7 +144,7 @@ def main():
     # )
 
     best_val_loss = float('inf')
-    best_path = f"gin_{args.dataset.lower()}/model_{args.dataset}_hidden_{args.model_size}_dropout_{args.dropout}_seed_{args.seed}.pth"
+    best_path = f"models/gin_{args.dataset.lower()}/model_{args.dataset}_hidden_{args.model_size}_dropout_{args.dropout}_seed_{args.seed}.pth"
     wait = 0
     # Training loop
     for epoch in trange(1, epochs + 1, desc="Epochs"):
